4b0f3e2c5be58c2674d185105ffe5ba7fa06345e
Mbem11 <74298724+Mbem11@users.noreply.github.com>
2020-11-19 16:09:35 +0100

    Update people.sol

--- PD-02/people.sol
+++ PD-02/people.sol
@@ -1,12 +1,7 @@
 pragma solidity 0.5.1;
 
 contract myContract {
-    Person[]public people;
-    
-    
-    return people.length;
-    
-    
+   
     address owner;
     
     modifier onlyOwner() {
@@ -23,15 +18,14 @@ contract myContract {
     constructor()public {
         owner = msg.sender;
     }
+  
+    Person[] public people;
     
-    function addPerson(string memory _firstName, string memory _lastName) public onlyOwner {
-      
-    }
-    mapping (uint => address) public onlyToOwner;
-    
-   unit [id] = people.push(Person(_firstName, _lastName)) + 1; {
-    unit id = people.length - 1; 
-    onlyToOwner [id] = msg.sender;
-      
+    function addPerson(string memory _firstName, string memory _lastName) public returns (uint) {
+        people.push(Person(_firstName, _lastName));
+        return  people.length - 1;
+   
     }
 }
+
+
