42690458342e647af1548bbc64c7627ab4afc153
Mbem11 <74298724+Mbem11@users.noreply.github.com>
2020-11-20 13:39:52 +0100

    Update people.sol

--- PD-02/people.sol
+++ PD-02/people.sol
@@ -4,6 +4,7 @@ contract myContract {
    
     address owner;
     
+    
     modifier onlyOwner() {
     require(msg.sender == owner);
     _;
@@ -12,20 +13,30 @@ contract myContract {
     struct Person {
       string _firstName;
       string _lastName;
+      uint _age;
       
     }
     
     constructor()public {
         owner = msg.sender;
+          
     }
-  
+ 
     Person[] public people;
     
-    function addPerson(string memory _firstName, string memory _lastName) public returns (uint) {
-        people.push(Person(_firstName, _lastName));
+    function addPerson(string memory _firstName, string memory _lastName, uint _age) public onlyOwner returns (uint) {
+        people.push(Person(_firstName, _lastName, _age));
         return  people.length - 1;
-   
+        
     }
+    
+    function adultTest(Person memory _people) public returns (string memory){
+    string memory adult;
+   if (_people.personAge >=18 == true)
+     adult = "You are a adult";
+    else
+  adult = "U are not a adult";
+    }
+   
+   
 }
-
-
