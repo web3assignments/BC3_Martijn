a62e1bc2eac888e4585eb0d462aef39e4e4307a6
Mbem11 <74298724+Mbem11@users.noreply.github.com>
2020-12-03 16:32:49 +0100

    Update Remix.sol

--- PD-03/Remix.sol
+++ PD-03/Remix.sol
@@ -1,9 +1,10 @@
 pragma solidity 0.5.1;
 
 contract MyContract {
-    mapping(address => uint) public balances
+    mapping(address => uint) public balances;
 
  address owner;
+ address payable wallet;
  
  uint startTime;
 
@@ -11,12 +12,7 @@ contract MyContract {
         require(block.timestamp >= startTime);
         _;
     }
- 
-    constructor() public{
-    owner = msg.sender;
-    startTime = 1606128173;
-    }
-    
+
     event Purchase(
         address indexed _buyer,
         uint _amount
@@ -24,6 +20,8 @@ contract MyContract {
 
     constructor(address payable _wallet) public {
         wallet = _wallet;
+         owner = msg.sender;
+    startTime = 1606128173;
     }
 
     function() external payable {
@@ -38,9 +36,4 @@ contract MyContract {
     }
 }
 
-    function buyToken() public payable {
-        balances[msg.sender] += 1;
-        wallet.transfer(msg.value);
-        emit Purchase(msg.sender, 1);
-    }
-}
+   
