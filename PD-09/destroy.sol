db328d3e5e5bea8529a079b4473d449063447ff5
Mbem11 <74298724+Mbem11@users.noreply.github.com>
2020-12-14 14:44:40 +0100

    Create destroy.sol

---
+++ PD-09/destroy.sol
@@ -0,0 +1,43 @@
+pragma solidity 0.5.1;
+
+
+contract MyContract {
+    mapping(address => uint) public balances;
+    
+function destroy() public {
+    selfdestruct(msg.sender);
+}
+
+ address owner;
+ address payable wallet;
+ 
+ uint startTime;
+
+    modifier onlyWhileOpen() {
+        require(block.timestamp >= startTime);
+        _;
+    }
+
+    event Purchase(
+        address indexed _buyer,
+        uint _amount
+    );
+
+    constructor(address payable _wallet) public {
+        wallet = _wallet;
+         owner = msg.sender;
+    startTime = 1606128173;
+    }
+
+
+    function() external payable {
+        buyToken();
+    }
+
+    function buyToken() public payable {
+        balances[msg.sender] += 1;
+        wallet.transfer(msg.value);
+        emit Purchase(msg.sender, 1);
+        
+    }
+}   
