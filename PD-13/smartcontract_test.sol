f6c8104d5484862a9275304df84f923b6732255d
Mbem11 <74298724+Mbem11@users.noreply.github.com>
2021-01-20 15:39:07 +0100

    Update smartcontract_test.sol

--- PD-13/smartcontract_test.sol
+++ PD-13/smartcontract_test.sol
@@ -6,28 +6,28 @@ import "../smartcontract.sol";
 contract testSuite {
 smartcontract mc;
 
-address constant recipient=0x0000000000000000000000000000000000000001;
-address constant bank=0x1A5CF1BF998df91829BAa128FcB89fE49eE9DB5D;
+address constant recipient=0x2967A60973344dF17758Dd35FC6248BfA12353b0;
+address constant bank=0x6A0943CfB2d00aB2B164A227534BE5fBdd49D605;
 address msgsender=address(this); // during tess this contract is the sender of the msg
 
     function beforeAll() public {
         mc=new smartcontract();
     }
 
-    function CheckUser() public {
-       Assert.equal(mc.user(),msgsender,"User should be address of this");
+    function CheckOwner() public {
+       Assert.equal(mc.owner(),msgsender,"Owner should be address of this");
     }
 
     function CheckBank() public  {
-       Assert.equal(mc.balances(bank),10000000,"Initial bank balance should be 10000000");
-       Assert.equal(mc.balances(msgsender),10000000,"Initial msgsender balance should be 10000000");
+       Assert.equal(mc.balances(bank),1,"Initial bank balance should be 1");
+       Assert.equal(mc.balances(msgsender),1,"Initial msgsender balance should be 1");
     }
     
-     function CheckSend() public  {
-       Assert.equal(mc.balances(recipient),0,"Initial recipient balance should be 0");
-       mc.send(recipient,25);
-       Assert.equal(mc.balances(recipient),25,"Recipient balance should now be 25");
-       Assert.equal(mc.balances(msgsender),10000000-25,"msgsender balance should be 10000000-25");
+     function CheckBuyToken() public  {
+       Assert.equal(mc.balances(recipient),1,"Initial recipient balance should be 1");
+       mc.buyToken(recipient,1);
+       Assert.equal(mc.balances(recipient),1,"Recipient balance should now be 1");
+       Assert.equal(mc.balances(msgsender),1+1,"msgsender balance should be 1+1");
     }
 
 }
