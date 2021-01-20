2772020e31797fe0b86066f04cd462a24e73e718
Mbem11 <74298724+Mbem11@users.noreply.github.com>
2021-01-20 15:52:05 +0100

    Update smartcontract_test.sol

--- PD-13/smartcontract_test.sol
+++ PD-13/smartcontract_test.sol
@@ -1,33 +1,25 @@
-pragma solidity >=0.4.22 <0.8.0;
-import "remix_tests.sol"; 
-import "remix_accounts.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.7.0;
+import "remix_tests.sol"; // injected by remix-tests
 import "../smartcontract.sol";
 
-contract testSuite {
-smartcontract mc;
+contract MyTest {
+  smartcontract foo;
 
-address constant recipient=0x2967A60973344dF17758Dd35FC6248BfA12353b0;
-address constant bank=0x6A0943CfB2d00aB2B164A227534BE5fBdd49D605;
-address msgsender=address(this); // during tess this contract is the sender of the msg
+  function beforeEach() public {
+    foo = new SimpleStorage();
+  }
 
-    function beforeAll() public {
-        mc=new smartcontract();
-    }
+  function initialValueShouldBe100() public returns (bool) {
+    return Assert.equal(foo.get(), 100, "initial value is not correct");
+  }
 
-    function CheckOwner() public {
-       Assert.equal(mc.owner(),msgsender,"Owner should be address of this");
-    }
+  function valueIsSet200() public returns (bool) {
+    foo.set(200);
+    return Assert.equal(foo.get(), 200, "value is not 200");
+  }
 
-    function CheckBank() public  {
-       Assert.equal(mc.balances(bank),1,"Initial bank balance should be 1");
-       Assert.equal(mc.balances(msgsender),1,"Initial msgsender balance should be 1");
-    }
-    
-     function CheckBuyToken() public  {
-       Assert.equal(mc.balances(recipient),1,"Initial recipient balance should be 1");
-       mc.buyToken(recipient,1);
-       Assert.equal(mc.balances(recipient),1,"Recipient balance should now be 1");
-       Assert.equal(mc.balances(msgsender),1+1,"msgsender balance should be 1+1");
-    }
-
+  function valueIsNotSet200() public returns (bool) {
+    return Assert.notEqual(foo.get(), 200, "value is 200");
+  }
 }
