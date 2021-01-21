6fc47708dca9d9cdea40cf9155aa66216267ba5d
Mbem11 <74298724+Mbem11@users.noreply.github.com>
2021-01-21 16:40:08 +0100

    Update smartcontract_test.sol

--- PD-13/smartcontract_test.sol
+++ PD-13/smartcontract_test.sol
@@ -22,4 +22,17 @@ contract MyTest {
   function valueIsNotSet200() public returns (bool) {
     return Assert.notEqual(foo.get(), 200, "value is 200");
   }
+  
+address constant recipient=0x2967A60973344dF17758Dd35FC6248BfA12353b0;
+address constant bank=0x6A0943CfB2d00aB2B164A227534BE5fBdd49D605;
+address msgsender=address(this); // during tess this contract is the sender of the msg
+
+    function beforeAll() public {
+        mc=new smartcontract();
+    }
+
+    function CheckOwner() public {
+       Assert.equal(mc.owner(),msgsender,"Owner should be address of this");
+    }
+  
 }
